3cab91ede33ccc342f7a0807f26618ac94354a3c
Lokeshwari-babu <logeshwaribabu05@gmail.com>
2024-07-13 19:56:04 +0530

    tst

---
+++ one.js
@@ -0,0 +1,14 @@
+let a = {}
+console.log(a)
+let emp = {
+    id: 101,
+    name: "rahul",
+    sal: 45000,
+}
+console.log(emp)
+console.log(emp.id)
+console.log(emp.name)
+console.log(emp.sal)
+emp.name = "rahul gandhi";
+emp.email = 'rg@pm.com';
+console.log(emp)
---
+++ two.js
@@ -0,0 +1,2 @@
+let arr = [10,20.5,"rahul",'y', true,false, undefined,null,[], {}]
+console.log(arr)
